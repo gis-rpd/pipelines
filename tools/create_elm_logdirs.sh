f5299de54d3b33d9561969394f10c00b6d417504
User Rig <userrig@gis01.(none)>
2016-06-04 00:18:11 +0800

    Fixed bug in tools/create_elm_logdirs.sh that always used elm-logs.testing

--- tools/create_elm_logdirs.sh
+++ tools/create_elm_logdirs.sh
@@ -4,7 +4,7 @@ pipeline=$1
 test -z "$pipeline" && exit 1
 
 for based in $RPD_ROOT/elm-logs $RPD_ROOT/elm-logs.testing; do
-    d=/mnt/projects/rpd/elm-logs.testing/$pipeline
+    d=$based/$pipeline
     echo "Creating $d (if needed) and setting perms as required"
     test -d $d || mkdir $d
     chmod 777 $d
