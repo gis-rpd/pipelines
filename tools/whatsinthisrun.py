502ee292d444dfe91e7cc1bcc319929dc05a55d1
Andreas WILM <wilma@gis.a-star.edu.sg>
2016-06-23 22:23:31 +0800

    Code review of tools/whatrunsforthislibrary.py and tools/whatsinthisrun.py

--- tools/whatsinthisrun.py
+++ tools/whatsinthisrun.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python3
+"""Print information about sequencing run
+"""
 
 import sys
 import requests
 
-# make sure exactly one arg given
-if len(sys.argv) != 2:
-	sys.exit("Must give only one run id")
 
-run_num = sys.argv[1]
-run_url = 'http://plap18v:8080/rest/seqrun/illumina/' + run_num + '/detailanalysis/json'
-print(run_url)
-r = requests.get(run_url)
-#r = requests.post(run_url, data = {"key":"value"})
-#r.status_code == requests.codes.ok
-if r.status_code != requests.codes.ok:
-	r.raise_for_status()
+def main(run_num):
+    """main function
+    """
 
-get_data = r.json()
-# if runId missing assume wrong run number
-if not get_data:
-	sys.exit("Run number NOT found in elm: check your runId")
+    run_url = 'http://plap18v:8080/rest/seqrun/illumina/' + run_num + '/detailanalysis/json'
+    print(run_url)
+    r = requests.get(run_url)
+    if r.status_code != requests.codes.ok:
+        r.raise_for_status()
 
-run_id = get_data['runId']
-print(run_id)
-#with open('config_library.txt', 'w') as fh_out:
-for rows in get_data['lanes']:	
+    get_data = r.json()
+    # if runId missing assume wrong run number
+    if 'runId' not in get_data:
+        sys.stderr.write("FATAL: Run number not found in ELM\n")
+        sys.exit(1)
 
-	if "MUX" in rows['libraryId']:
-		for child in rows['Children']:
-			sample = (get_data['runId'],rows['laneId'],rows['libraryId'],'\tNA',child['barcode'],child['genome'],child['libraryId'],child['libtech'],child['Analysis'])
-			print('\t'.join(sample))
-			#sample = get_data['runId'] + '\t' + rows['laneId'] + '\t' + rows['libraryId'] + '\tNA\t' + child['barcode'] + '\t' + child['genome'] + '\t' + child['libraryId'] + '\t' + child['libtech'] + '\t' + child['Analysis']
-			#print(sample)
-			    #fh_out.write(sample + '\n')
-	else:
-		sample = (run_num,rows['laneId'],rows['libraryId'],rows['genome'],rows['libtech'],rows['Analysis'])
-		print('\t'.join(sample))
-		#sample = run_num + '\t' + rows['laneId'] + '\t' + rows['libraryId'] + '\t' + rows['genome'] + '\t' + rows['libtech'] + '\t' + rows['Analysis']
-		#print(sample)
-		    #fh_out.write(sample + '\n')
+    run_id = get_data['runId']
+    print(run_id)
+    for rows in get_data['lanes']:
+        if "MUX" in rows['libraryId']:
+            for child in rows['Children']:
+                sample = (get_data['runId'], rows['laneId'], rows['libraryId'], '\tNA', child['barcode'], child['genome'], child['libraryId'], child['libtech'], child['Analysis'])
+                print('\t'.join(sample))
+        else:
+            sample = (run_num, rows['laneId'], rows['libraryId'], rows['genome'], rows['libtech'], rows['Analysis'])
+            print('\t'.join(sample))
+
+
+if __name__ == "__main__":
+    # FIXME:add-argparser
+    if len(sys.argv) != 2:
+        sys.stderr.write("FATAL: need exactly only run id\n")
+        sys.exit(1)
+    run_num = sys.argv[1]
