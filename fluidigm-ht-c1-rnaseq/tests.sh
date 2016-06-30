900f3e9962b3262c1a916e691df35bb21f2ad0f5
Andreas WILM -CSB5 <wilma@gis.a-star.edu.sg>
2016-06-30 16:37:11 +0800

    fluidigm-ht-c1-rnaseq: - added tests - tuned cluster config - meaningful values to RG - added R as module - logging on

---
+++ fluidigm-ht-c1-rnaseq/tests.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# FIXME reimplement in python
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
+MYNAME=$(basename $(readlink -f $0))
+
+toaddr() {
+    if [ $(whoami) == 'userrig' ]; then
+        echo "rpd@mailman.gis.a-star.edu.sg";
+    else
+        echo "$(whoami)@gis.a-star.edu.sg";
+    fi
+}
+
+usage() {
+    echo "$MYNAME: run pipeline tests"
+    echo " -d: Run dry-run tests"
+    echo " -r: Run real-run tests"
+}
+skip_dry_runs=1
+skip_real_runs=1
+while getopts "dr" opt; do
+    case $opt in
+        d)
+            skip_dry_runs=0
+            ;;
+        r)
+            skip_real_runs=0
+            ;;
+        \?)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+
+# readlink resolves links and makes path absolute
+test -z "$RPD_ROOT" && exit 1
+
+FQDIR=$RPD_ROOT/testing/data/fluidigm_800_singlecell/K562_Fldm/;
+COL1_R1=$FQDIR/COL01_S1_L001_R1_001.fastq.gz
+COL1_R2=$FQDIR/COL01_S1_L001_R2_001.fastq.gz
+
+cd $(dirname $0)
+pipeline=$(pwd | sed -e 's,.*/,,')
+commit=$(git describe --always --dirty)
+test -e Snakefile || exit 1
+
+
+test_outdir_base=$RPD_ROOT/testing/output/${pipeline}/${pipeline}-commit-${commit}
+log=$(mktemp)
+COMPLETE_MSG="*** All tests completed ***"
+echo "Logging to $log"
+echo "Check log if the following final message is not printed: \"$COMPLETE_MSG\""
+
+
+WRAPPER=./fluidigm-ht-c1-rnaseq.py
+cmd_base="$WRAPPER -1 $COL1_R1 -2 $COL1_R2 -s COL01"
+
+# dryruns
+#
+if [ $skip_dry_runs -ne 1 ]; then
+    echo "Dryrun" | tee -a $log
+    odir=$(mktemp -d ${test_outdir_base}.XXXXXXXXXX) && rmdir $odir
+    eval $cmd_base -o $odir -v --no-run >> $log 2>&1
+    pushd $odir >> $log
+    EXTRA_SNAKEMAKE_ARGS="--dryrun" bash run.sh >> $log 2>&1
+    rm -rf $odir
+    popd >> $log
+    
+else
+    echo "Dryruns tests skipped"
+fi
+
+
+# real runs
+#
+if [ $skip_real_runs -ne 1 ]; then
+    echo "Realrun" | tee -a $log
+    odir=$(mktemp -d ${test_outdir_base}.XXXXXXXXXX) && rmdir $odir
+    eval $cmd_base -o $odir -v >> $log 2>&1
+    # magically works even if line just contains id as in the case of pbspro
+    jid=$(tail -n 1 $odir/logs/submission.log  | cut -f 3 -d ' ')
+    echo "Started job $jid writing to $odir. You will receive an email"
+    
+else
+    echo "Real-run test skipped"
+fi
+
+
+echo
+echo "$COMPLETE_MSG"
+
