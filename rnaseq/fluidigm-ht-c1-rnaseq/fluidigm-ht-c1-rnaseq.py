015509b6cdb64a660a46ba76ecf6252cdde3e063
Andreas WILM -CSB5 <wilma@gis.a-star.edu.sg>
2016-07-29 14:46:04 +0800

    rnaseq/fluidigm-ht-c1-rnaseq/ and rnaseq/star-rsem now also multi-sample ready with unified wrapper interface

--- rnaseq/fluidigm-ht-c1-rnaseq/fluidigm-ht-c1-rnaseq.py
+++ rnaseq/fluidigm-ht-c1-rnaseq/fluidigm-ht-c1-rnaseq.py
@@ -24,12 +24,13 @@ LIB_PATH = os.path.abspath(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "lib"))
 if LIB_PATH not in sys.path:
     sys.path.insert(0, LIB_PATH)
+from readunits import get_samples_and_readunits_from_cfgfile
+from readunits import get_readunits_from_args
 from pipelines import get_pipeline_version
-from pipelines import ref_is_indexed
-from pipelines import get_site
 from pipelines import PipelineHandler
+from pipelines import get_site
 from pipelines import logger as aux_logger
-from readunits import get_reads_unit_from_cfgfile, get_reads_unit_from_args, key_for_readunit
+#from pipelines import ref_is_indexed
 
 
 __author__ = "Andreas Wilm"
@@ -52,6 +53,7 @@ DEFAULT_SLAVE_Q = {'GIS': None,
 DEFAULT_MASTER_Q = {'GIS': None,
                     'NSCC': 'production'}
 
+# global logger
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(
@@ -59,25 +61,17 @@ handler.setFormatter(logging.Formatter(
 logger.addHandler(handler)
 
 
-
 def main():
     """main function
     """
+
     parser = argparse.ArgumentParser(description=__doc__.format(
         PIPELINE_NAME=PIPELINE_NAME, PIPELINE_VERSION=get_pipeline_version()))
-    parser.add_argument('-1', "--fq1", nargs="+",
-                        help="FastQ file/s (gzip only)."
-                        " Multiple input files supported (auto-sorted)."
-                        " Note: each file gets a unique read group id assigned."
-                        " Collides with -c.")
-    parser.add_argument('-2', "--fq2", nargs="+",
-                        help="FastQ file/s (if paired) (gzip only). See also --fq1")
-    parser.add_argument('-s', "--sample", required=True,
-                        help="Sample name")
+
+    # generic args
     parser.add_argument('-c', "--config",
-                        help="Config file (YAML) listing: run-, flowcell-, sample-id, lane"
-                        " as well as fastq1 and fastq2 per line. Will create a new RG per line,"
-                        " unless read groups is set in last column. Collides with -1, -2")
+                        help="Config file (YAML) listing samples and readunits."
+                        " Collides with -1, -2 and -s")
     parser.add_argument('-o', "--outdir", required=True,
                         help="Output directory (may not exist)")
     parser.add_argument('--no-mail', action='store_true',
@@ -90,8 +84,21 @@ def main():
     parser.add_argument('-m', '--master-q', default=default,
                         help="Queue to use for master job (default: {})".format(default))
     parser.add_argument('-n', '--no-run', action='store_true')
-    parser.add_argument('-v', '--verbose', action='count', default=1)
-    parser.add_argument('-q', '--quiet', action='count', default=0)
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help="Increase verbosity")
+    parser.add_argument('-q', '--quiet', action='count', default=0,
+                        help="Decrease verbosity")
+
+    # pipeline specific args
+    parser.add_argument('-1', "--fq1", nargs="+",
+                        help="FastQ file/s (gzip only)."
+                        " Multiple input files supported (auto-sorted)."
+                        " Note: each file (or pair) gets a unique read-group id."
+                        " Collides with -c.")
+    parser.add_argument('-2', "--fq2", nargs="+",
+                        help="FastQ file/s (if paired) (gzip only). See also --fq1")
+    parser.add_argument('-s', "--sample",
+                        help="Sample name. Collides with -c.")
 
     args = parser.parse_args()
 
@@ -107,46 +114,46 @@ def main():
     logger.setLevel(logging.WARN + 10*args.quiet - 10*args.verbose)
     aux_logger.setLevel(logging.WARN + 10*args.quiet - 10*args.verbose)
 
-    # FIXME checks on reffa index (currently not exposed via args)
-    
-    if args.config:
-        if any([args.fq1, args.fq2]):
-            logger.fatal("Config file overrides fastq input arguments. Use one or the other")
-            sys.exit(1)
-        if not os.path.exists(args.config):
-            logger.fatal("Config file %s does not exist", args.config)
-            sys.exit(1)
-        read_units = get_reads_unit_from_cfgfile(args.config)
-    else:
-        read_units = get_reads_unit_from_args(args.fq1, args.fq2)
-
-    for i, ru in enumerate(read_units):
-        logger.debug("Checking read unit #%d: %s", i, ru)
-        for f in [ru.fq1, ru.fq2]:
-            if f and not os.path.exists(f):
-                logger.fatal("Non-existing input file %s", f)
-                sys.exit(1)
-
     if os.path.exists(args.outdir):
         logger.fatal("Output directory %s already exists", args.outdir)
         sys.exit(1)
 
+    # samples is a dictionary with sample names as key (mostly just
+    # one) and readunit keys as value. readunits is a dict with
+    # readunits (think: fastq pairs with attributes) as value
+    if args.config:
+        if any([args.fq1, args.fq2, args.sample]):
+            logger.fatal("Config file overrides fastq and sample input arguments."
+                         " Use one or the other")
+            sys.exit(1)
+        if not os.path.exists(args.config):
+            logger.fatal("Config file %s does not exist", args.config)
+            sys.exit(1)
+        samples, readunits = get_samples_and_readunits_from_cfgfile(args.config)
+    else:
+        if not all([args.fq1, args.sample]):
+            logger.fatal("Need at least fq1 and sample without config file")
+            sys.exit(1)
+
+        readunits = get_readunits_from_args(args.fq1, args.fq2)
+        # all readunits go into this one sample specified on the command-line
+        samples = dict()
+        samples[args.sample] = list(readunits.keys())
+
+    # FIXME checks on reffa index (currently not exposed via args)
 
     # turn arguments into user_data that gets merged into pipeline config
-    user_data = {'mail_on_completion': not args.no_mail}
-    user_data['readunits'] = dict()
-    for ru in read_units:
-        k = key_for_readunit(ru)
-        user_data['readunits'][k] = dict(ru._asdict())
-
-    # samples is a dictionary with sample names as key (here just one)
-    # each value is a list of readunits
-    user_data['samples'] = dict()
-    user_data['samples'][args.sample] = list(user_data['readunits'].keys())
+    #
+    # generic data first
+    user_data = dict()
+    user_data['mail_on_completion'] = not args.no_mail
+    user_data['readunits'] = readunits
+    user_data['samples'] = samples
 
     pipeline_handler = PipelineHandler(
-        PIPELINE_NAME, PIPELINE_BASEDIR, args.outdir, user_data,
-        site=site, master_q=args.master_q, slave_q=args.slave_q)
+        PIPELINE_NAME, PIPELINE_BASEDIR,
+        args.outdir, user_data, site=site,
+        master_q=args.master_q, slave_q=args.slave_q)
     pipeline_handler.setup_env()
     pipeline_handler.submit(args.no_run)
 
