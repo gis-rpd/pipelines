eca7562e483b7c52d9a72815a9aa92654d3402e6
cfncluster user <ec2-user@ip-172-31-7-30.ap-southeast-1.compute.internal>
2017-01-26 08:18:07 +0000

    tools/create_pipeline_dirs.sh: now creating elm and downstream dirs based on env vars and site.yaml

--- tools/create_pipeline_dirs.sh
+++ tools/create_pipeline_dirs.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-pipeline=$1
-test -z "$pipeline" && exit 1
+pipelines=$@
+test -z "$pipelines" && exit 1
 
-for based in $RPD_ROOT/elm-logs $RPD_ROOT/elm-logs.testing; do
-    d=$based/$pipeline
-    echo "Creating $d (if needed) and setting perms as required"
-    test -d $d || mkdir $d
+# elm logging dirs
+#
+for m in rpd/testing rpd/production; do
+  module load $m
+  for p in $pipelines; do
+    d=$RPD_ELMLOGDIR/$p
+#cat<<EOF
+    test -d $d || mkdir -p $d 2>/dev/null
     chmod 777 $d
     chmod g+s $d
+#EOF
+  done
 done
 
-for based in $RPD_ROOT/testing/output/; do
-    d=$based/$pipeline
-    echo "Creating $d (if needed) and setting perms as required"
-    test -d $d || mkdir $d
-    chmod 775 $d
-    chmod g+s $d
+# downstream dirs
+#
+for outdir in $(grep -A2 downstream_outdir_base etc/site.yaml  | awk '/(devel|production):/ {print $NF}'); do
+      for p in $pipelines; do
+	  d=$outdir/$p
+	  echo "Creating $d (if needed) and setting perms as required"
+	  test -d $d || mkdir -p $d 2>/dev/null
+	  chmod 775 $d
+	  chmod g+s $d
+      done
 done
