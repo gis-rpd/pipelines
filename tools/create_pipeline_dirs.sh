1a3705b0715ddd3ea49de4e056e85f384eec6067
cfncluster user <ec2-user@ip-172-31-23-148.ap-southeast-1.compute.internal>
2017-02-13 07:29:00 +0000

    Removed unnecessary and wrong downstream dir creation

--- tools/create_pipeline_dirs.sh
+++ tools/create_pipeline_dirs.sh
@@ -19,12 +19,10 @@ done
 
 # downstream dirs
 #
-for outdir in $(grep -A2 downstream_outdir_base etc/site.yaml  | awk '/(devel|production):/ {print $NF}'); do
-      for p in $pipelines; do
-	  d=$outdir/$p
-	  echo "Creating $d (if needed) and setting perms as required"
-	  test -d $d || mkdir -p $d 2>/dev/null
-	  chmod 775 $d
-	  chmod g+s $d
-      done
+for d in $(grep -A2 downstream_outdir_base etc/site.yaml  | awk '/(devel|production):/ {print $NF}'); do
+    # next level is user. so only need to be sure this toplevel is writeable
+    echo "Creating $d (if needed) and setting perms as required"
+    test -d $d || mkdir -p $d 2>/dev/null
+    chmod 775 $d
+    chmod g+s $d
 done
