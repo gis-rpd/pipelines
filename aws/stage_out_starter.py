c0b1439cbe9ee819820ab5ab3a0ff4826f272152
cfncluster user <ec2-user@ip-172-31-23-148.ap-southeast-1.compute.internal>
2017-02-13 07:48:42 +0000

    aws/stage_out_starter.py: fixed logging error and now using downstream_dir based on production or devel

--- aws/stage_out_starter.py
+++ aws/stage_out_starter.py
@@ -21,7 +21,9 @@ LIB_PATH = os.path.abspath(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "lib"))
 if LIB_PATH not in sys.path:
     sys.path.insert(0, LIB_PATH)
-from pipelines import DOWNSTREAM_OUTDIR_TEMPLATE, WORKFLOW_COMPLETION_FLAGFILE
+from pipelines import DOWNSTREAM_OUTDIR_TEMPLATE
+from pipelines import WORKFLOW_COMPLETION_FLAGFILE
+from pipelines import is_devel_version
 from config import site_cfg
 
 
@@ -49,7 +51,12 @@ def main():
     # See https://www.reddit.com/r/Python/comments/3nctlm/what_python_tools_should_i_be_using_on_every/
     logger.setLevel(logging.WARN + 10*args.quiet - 10*args.verbose)
 
-    basedir = site_cfg['downstream_outdir_base']['production']#'devel'
+    # only makes sense for production if run as cron
+    if is_devel_version():
+        k = 'devel'
+    else:
+        k = 'production'
+    basedir = site_cfg['downstream_outdir_base'][k]
     dirglob = DOWNSTREAM_OUTDIR_TEMPLATE.format(
         basedir=basedir, user="*", pipelineversion="*", pipelinename="*", timestamp="*")
     logger.debug("dirglob is %s", dirglob)
@@ -57,7 +64,7 @@ def main():
     for flagfile in glob.glob(os.path.join(
             dirglob, WORKFLOW_COMPLETION_FLAGFILE)):
         dir = os.path.dirname(flagfile)
-        logger.verbose("Starting staging out of %s", dir)
+        logger.info("Starting staging out of %s", dir)
         try:
             cmd = [STAGE_OUT_WORKER, '-r', dir]
             _ = subprocess.check_output(cmd)
