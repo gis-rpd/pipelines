d9a5b6d2c63890113ff712f7038916fe41105605
cfncluster user <ec2-user@ip-172-31-24-251.ap-southeast-1.compute.internal>
2017-02-24 15:55:51 +0000

    aws/handle_unreachable_instances.sh: fixed killing of pingable hosts instead of other way around

--- aws/handle_unreachable_instances.sh
+++ aws/handle_unreachable_instances.sh
@@ -5,7 +5,7 @@ set -u
 
 
 for host in $(qhost | awk 'NR>3 {print $1}'); do 
-    ping -c 10 $host >/dev/null || continue;
+    ping -c 10 $host >/dev/null && continue;
     msg="Removing non pingeable host $host from UGE"
     logger -s -t $(basename $0) "$msg"
 
