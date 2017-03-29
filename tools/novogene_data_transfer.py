de4966225494096dadcd4cc3e0da298a644ef7ec
Lavanya VEERAVALLI <veeravallil@gis.a-star.edu.sg>
2017-03-29 22:58:45 +0800

    jobs insert function (pipeline_runs collection) added and to be tested with downstream starter.py

--- tools/novogene_data_transfer.py
+++ tools/novogene_data_transfer.py
@@ -7,6 +7,8 @@ import sys
 import os
 import argparse
 import subprocess
+import glob
+from collections import namedtuple
 
 #--- third party imports
 #
@@ -21,9 +23,12 @@ if LIB_PATH not in sys.path:
     sys.path.insert(0, LIB_PATH)
 from mongodb import mongodb_conn
 from pipelines import is_production_user, is_devel_version
-from pipelines import generate_window
+from pipelines import generate_window, get_machine_run_flowcell_id
 from config import novogene_conf
+from readunits import key_for_readunit
 
+ReadUnit = namedtuple('ReadUnit', ['run_id', 'flowcell_id', 'library_id',
+                                   'lane_id', 'rg_id', 'fq1', 'fq2'])
 __author__ = "Lavanya Veeravalli"
 __email__ = "veeravallil@gis.a-star.edu.sg"
 __copyright__ = "2016 Genome Institute of Singapore"
@@ -36,8 +41,9 @@ handler.setFormatter(logging.Formatter(
     '[{asctime}] {levelname:8s} {filename} {message}', style='{'))
 logger.addHandler(handler)
 
-def runs_from_db(db, win=14):
+def runs_from_db(connection, win=14):
     """Get the runs from pipeline_run collections"""
+    db = connection.gisds.runcomplete
     epoch_present, epoch_back = generate_window(win)
     results = db.find({"run" : {"$regex" : "^NG00"},
                        "timestamp": {"$gt": epoch_back, "$lt": epoch_present}})
@@ -48,7 +54,7 @@ def runs_from_db(db, win=14):
         if not record.get('analysis'):
             logger.critical("run is missing for DB-id %s", record['_id'])
             continue
-        mux_list = {}
+        run_records = {}
         for (analysis_count, analysis) in enumerate(record['analysis']):
             analysis_id = analysis['analysis_id']
             per_mux_status = analysis.get("per_mux_status", None)
@@ -71,57 +77,108 @@ def runs_from_db(db, win=14):
                     analysis_count, mux_count)
                 if mux_status.get('Status') == "SUCCESS" and \
                     mux_status.get('DownstreamSubmission') == "TODO":
-                    mongo_list = (run_number, downstream_id, analysis_id, out_dir)
-                    if mux_id in mux_list:
+                    mux_info = (run_number, downstream_id, analysis_id, out_dir)
+                    if mux_id in run_records:
                         #Send email the above message
-                        logger.info("MUX %s from %s has been analyzed more than 1 time succeessfully,\
-                            send email", mux_id, run_number)
-                        del mux_list[mux_id]
+                        logger.info("MUX %s from %s has been analyzed more than 1 time \
+                            succeessfully, send email", mux_id, run_number)
+                        del run_records[mux_id]
                     else:
-                        mux_list[mux_id] = mongo_list
-        if mux_list:
-            yield mux_list
+                        run_records[mux_id] = mux_info
+        if run_records:
+            yield run_records
 
-def update_mongodb(db, run_number, analysis_id, downstream_id, Status):
+def update_downstream_mux(connection, run_number, analysis_id, downstream_id, Status):
     """Update the status in the mongoDB runcomplete collection
     """
+    db = connection.gisds.runcomplete
     try:
         db.update({"run": run_number, 'analysis.analysis_id' : analysis_id},
                   {"$set": {downstream_id: Status,}})
     except pymongo.errors.OperationFailure:
         logger.fatal("MongoDB OperationFailure")
-        sys.exit(0)
+        sys.exit(1)
 
-def start_data_transfer(db, mux, info, site):
+def insert_muxjob(connection, mux, job):
+    """Insert records into pipeline_runs collection of MongoDB
+    """
+    try:
+        db = connection.gisds.pipeline_runs_copy
+        db.insert_one(job)
+        logger.info("Job inserted for MUX %s", mux)
+    except pymongo.errors.OperationFailure:
+        logger.fatal("mongoDB OperationFailure")
+        sys.exit(1)
+
+def get_mux_details(run_number, mux_id, fastq_dest):
+    sample_list = glob.glob(os.path.join(fastq_dest, "*"+ mux_id, 'Sample_*'))
+    _, _, flowcellid = get_machine_run_flowcell_id(run_number)
+    sample_info = {}
+    readunits_dict = {}
+    samples_dict = {}
+    for sample in sample_list:
+        sample_fastq_list = glob.glob(os.path.join(sample, '*fastq.gz'))
+        #Check if R1 and R2 pairs for each lane are equal
+        if len(sample_fastq_list)%2 == 0:
+            lane_dict = {}
+            for x in sorted(sample_fastq_list):
+                lane_id = os.path.basename(x).split('_')
+                indices = [i for i, s in enumerate(lane_id) if 'L00' in s]
+                lane_dict.setdefault(lane_id[indices[0]][-1], []).append(x)
+        for lane, v in lane_dict.items():
+            lib = (os.path.basename(sample).split('_')[1])
+            R1 = [i for i, s in enumerate(v) if '_R1_' in s]
+            R2 = [i for i, s in enumerate(v) if '_R2_' in s]
+            fq1 = v[R1[0]]
+            fq2 = v[R2[0]]
+            ru = ReadUnit(run_number, flowcellid, lib, lane, None, fq1, fq2)
+            k = key_for_readunit(ru)
+            readunits_dict[k] = dict(ru._asdict())
+            sample_info['readunits'] = readunits_dict
+            samples_dict.setdefault(lib, []).append(k)
+            sample_info['samples'] = samples_dict
+    return sample_info
+
+def start_data_transfer(connection, mux, mux_info, site):
     """ Data transfer from source to destination
     """
-    bcl_path = info[3]
-    mux_fastq_path = os.path.join(bcl_path, "out", "Project_"+mux)
-    bcl = os.path.basename(info[3])
-    run_number = info[0]
-    analysis_id = info[2]
-    downstream_id = info[1]
-    fastq_dest = os.path.join(novogene_conf['FASTQ_DEST'][site], mux, run_number, bcl)
-    rsync_cmd = '/usr/bin/rsync -va %s %s' % (mux_fastq_path, fastq_dest)
+    bcl_path = mux_info[3]
+    fastq_src = os.path.join(bcl_path, "out", "Project_"+mux)
+    bcl_dir = os.path.basename(mux_info[3])
+    run_number = mux_info[0]
+    analysis_id = mux_info[2]
+    downstream_id = mux_info[1]
+    fastq_dest = os.path.join(novogene_conf['FASTQ_DEST'][site], mux, run_number, bcl_dir)
+    rsync_cmd = '/usr/bin/rsync -va %s %s' % (fastq_src, fastq_dest)
     if not os.path.exists(fastq_dest):
         try:
-            #Change the mongoDB status
             os.makedirs(fastq_dest)
             logger.info("data transfer started for %s from %s", mux, run_number)
-            update_mongodb(db, run_number, analysis_id, downstream_id, "COPYING")
+            update_downstream_mux(connection, run_number, analysis_id, downstream_id, "COPYING")
             _ = subprocess.check_output(rsync_cmd, shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             logger.fatal("The following command failed with return code %s: %s",
                          e.returncode, ' '.join(rsync_cmd))
             logger.fatal("Output: %s", e.output.decode())
             logger.fatal("Exiting")
-            update_mongodb(db, run_number, analysis_id, downstream_id, "ERROR")
+            #Send_mail
+            #Delete the partial info being rsync
+            update_downstream_mux(connection, run_number, analysis_id, downstream_id, "ERROR")
             sys.exit(1)
         #Update the mongoDB for successful data transfer
-        #Update jobs (multisample) in pipeline_runs collection and replace the
-        #jobid instead of SUCCESS in the following mongo status update
+        sample_info = get_mux_details(run_number, mux, fastq_dest)
+        job = {}
+        job['sample_cfg'] = {}
+        for outer_key, outer_value in sample_info.items():
+            ctime, _ = generate_window(1)
+            job['sample_cfg'].update({outer_key:outer_value})
+            job['site'] = site
+            job['pipeline_name'] = 'custom/SG10K'
+            job['pipeline_version'] = 'current'
+            job['ctime'] = ctime
         logger.info("data transfer successfully completed for %s from %s", mux, run_number)
-        update_mongodb(db, run_number, analysis_id, downstream_id, "SUCCESS")
+        insert_muxjob(connection, mux, job)
+        update_downstream_mux(connection, run_number, analysis_id, downstream_id, "SUCCESS")
         return True
     else:
         return False
@@ -162,21 +219,23 @@ def main():
     connection = mongodb_conn(args.testing)
     if connection is None:
         sys.exit(1)
-    db = connection.gisds.runcomplete
     if is_devel_version() or args.testing:
         mail_to = 'veeravallil'# domain added in mail function
     else:
         mail_to = 'rpd'
-    run_records = runs_from_db(db, args.win)
+    run_records = runs_from_db(connection, args.win)
     trigger = 0
     for run in run_records:
-        for mux, info in run.items():
-            find = start_data_transfer(db, mux, info, site='nscc')
-            if find:
+        for mux, mux_info in run.items():
+            if args.dry_run:
+                logger.warning("Skipping job delegation for %s", mux)
+                continue
+            res = start_data_transfer(connection, mux, mux_info, site='nscc')
+            if res:
                 trigger = 1
             else:
-                #send email alert
-                logger.warning("%s from %s, already exists, please check", mux, info[0])
+                #send_mail alert
+                logger.warning("%s from %s, already exists, please check", mux, mux_info[0])
                 continue
         if args.break_after_first and trigger == 1:
             logger.info("Stopping after first run")
