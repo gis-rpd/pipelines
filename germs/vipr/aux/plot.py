202a8e56b8da909e734f68bb075b99c5503f246b
Andreas WILM -CSB5 <wilma@gis.a-star.edu.sg>
2018-04-05 11:14:22 +0800

    Enhancements for ViPR
    - simple contig joined now output bed showing coordinates of reference filled gaps
    - reference filled gaps can now be plotted in the plotting routines
    - added early fake reference index dependency which effectively checks valid format eaarly
    - adjusted cluster limits

--- germs/vipr/aux/plot.py
+++ germs/vipr/aux/plot.py
@@ -66,7 +66,36 @@ def af_from_vcf(vcf_gz, snps_only=False):
     return afs
 
 
-def plot(cov_gzfile, vcf_gzfile, plot_file):
+def parse_bed(fn):
+    """Parses bed file fn and returns first three columns is iterator
+
+    Adapted from LoFreq 2.1.2
+    """
+    with open(fn, 'r') as fh:
+        for line in fh:            
+            if line.startswith('#') or len(line.strip()) == 0:
+                continue
+            
+            # bed should use tab as delimiter. use whitespace if tab fails.
+            chrom = start = end = None
+            for delim in ['\t', '']:
+                ls = line.strip().split(delim)
+                if len(ls) >= 3:
+                    (chrom, start, end) = ls[0:3]
+                    break
+                if not chrom:
+                    raise ValueError("Couldn't parse the following line"
+                                     " from bed-file %s: %s" % (fn, line))
+                
+            # int(float()) allows for scientific notation
+            (start, end) = [int(float(x)) for x in [start, end]]
+            if end <= start or end < 0 or start < 0:
+                raise ValueError("Invalid coordinates in the following line"
+                                 " from bed-file %s: %s" % (fn, line))
+            yield (chrom, start, end)
+
+            
+def plot(cov_gzfile, vcf_gzfile, plot_fn, bed_fn=None):
     """read cov and vcf and creates plot"""
     genomecov = parse_genomecov(cov_gzfile)
     afs = af_from_vcf(vcf_gzfile, snps_only=True)
@@ -87,7 +116,7 @@ def plot(cov_gzfile, vcf_gzfile, plot_file):
 
     ylabel_size = 'medium'
     c = 'blue'
-    ax1.plot(x1, y1, color=c)
+    lgd_cov = ax1.plot(x1, y1, color=c, label="Coverage")
     ax1.set_ylabel('Coverage', color=c, size=ylabel_size, weight='bold')
     #ax1.tick_params('y', color=c)
     ax1.set_yscale('log', basex=10)
@@ -97,17 +126,56 @@ def plot(cov_gzfile, vcf_gzfile, plot_file):
     #ax1.grid(True, which='x')
     plt.gca().xaxis.grid(True)
 
-    c = 'red'
     ax2 = ax1.twinx()
-    ax2.scatter(x2, y2, color=c)
-    ax2.set_ylabel('SNP AF', color=c, size=ylabel_size, weight='bold')
+
+    lgd_gaps = None
+    # plot bed on ax2 since non log scaled
+    if bed_fn:
+        gap_fill_coords = list(parse_bed(bed_fn))
+        if len(gap_fill_coords):
+            bed_sq = set([c[0] for c in gap_fill_coords])
+            # ignoring chrom here. assuming 1 chrom only
+            assert len(bed_sq)==1, ("Expected exactly one SQ in bed %s" % bed_fn)
+            bed_sq = list(bed_sq)[0]
+            assert bed_sq in afs, ("Bed SQ %s not found in AFs: %s" % (
+                bed_sq, ', '.join(afs.keys())))
+
+            c = 'green'
+            #xmin = [c[1] for c in gap_fill_coords]
+            #xmax = [c[2] for c in gap_fill_coords]
+            #lgd_gaps = ax2.hlines([1]*len(xmin), xmin, xmax, colors=c,
+            #                     linestyles='solid', lw=10, label='Gaps')
+            for gfc in gap_fill_coords:
+                xmin, xmax = gfc[1], gfc[2]
+                lgd_gaps = ax2.axvspan(xmin, xmax, facecolor=c,
+                                       alpha=0.3, label='Filled Gaps')
+                # storing one is sufficient for legend
+            
+    c = 'red'
+    lgd_vars = ax2.scatter(x2, y2, color=c, label="Variants")
+    ax2.set_ylabel('Allele Freq.', color=c, size=ylabel_size, weight='bold')
     ax2.set_ylim(bottom=0, top=1)
     #ax2.tick_params('y', color=c)
 
-    #fig.tight_layout()
+
+    # weird hack to get the legend of two axis into one. See
+    # https://stackoverflow.com/questions/5484922/secondary-axis-with-twinx-how-to-add-to-legend
+    labs = []
+    lobs = []
+    for l in [lgd_cov, lgd_gaps, lgd_vars]:
+        if l is None:
+            continue
+        try:
+            lbl = l.get_label()
+            lobs.append(l)
+        except AttributeError:
+            lbl = l[0].get_label()
+            lobs.append(l[0])
+        labs.append(lbl)        
+    ax2.legend(lobs, labs, bbox_to_anchor=(1.1, 1), loc=2, borderaxespad=0.)
 
     plt.title(sq)
-    plt.savefig(plot_file)
+    plt.savefig(plot_fn, bbox_inches="tight")
 
 
 def main():
@@ -119,10 +187,12 @@ def main():
                         help="bedtools genomecov file (gzipped)")
     parser.add_argument("-p", "--plot", required=True,
                         help="Output plot filename (pdf format!).")
+    parser.add_argument("-b", "--bed",
+                        help="Bed file listing positions gap-filled with reference")
     args = parser.parse_args()
 
     assert not os.path.exists(args.plot)
-    plot(args.cov, args.vcf, args.plot)
+    plot(args.cov, args.vcf, args.plot, bed_fn=args.bed)
 
 
 if __name__ == "__main__":
